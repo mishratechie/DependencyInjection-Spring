f60227a9af84079535de54ee22ba75f28cf9addf
Ashutosh <mishra.techie@gmail.com>
2016-07-23 12:25:52 +0530

    added author details

---
+++ src/main/java/com/SimpleAnnotation/ClassA.java
@@ -0,0 +1,13 @@
+package com.SimpleAnnotation;
+
+/**
+ * Created by Ashutosh on 7/20/2016.
+ * Please contact mishra.techie@gmail.com with your queries
+ */
+public class ClassA {
+
+    public void printMeA(){
+
+        System.out.println("Hello am from classA");
+    }
+}
